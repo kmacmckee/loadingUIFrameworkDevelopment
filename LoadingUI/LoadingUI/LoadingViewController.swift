b1be552e24649d40b6dc8174ac524156234de81b
Kobe McKee <kobe.mckee@gmail.com>
2019-06-26 12:50:11 -0600

    Initial commit

---
+++ LoadingUI/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Kobe McKee on 6/26/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Kobe McKee on 6/26/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
